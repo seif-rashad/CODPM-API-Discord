7c52dd7fd8560396aca32bdfb715ed7a4b4192b6
= <=>
2024-05-15 00:37:27 +0300

    - Added DiscordJs - Used DiscordJs WebHook client instead of directly sending the request to the API - Added auto-update the same message instead of sending a new one every time

--- CODPM-API-Discord.js
+++ CODPM-API-Discord.js
@@ -1,4 +1,6 @@
-const DISCORD_WEBHOOK = new URL('');
+const { WebhookClient } = require("discord.js");
+const DISCORD_WEBHOOK = new WebhookClient({ id: "1239983856640196808", token: "NQoV5pDC5Zw-zVfBIlewSJTFoLx8srWsCsC4m9V4BeKBQ7OdCqrLUgnu5ldyWOCcmsFs" });
+let LAST_MESSAGE_ID = null;
 const DISCORD_TABLELAYOUT = 'ansi'; // 'ansi' or 'unicode'
 const DISCORD_MAXEMBEDS = 5;
 const CODPM_GAME = 'cod'; // cod, coduo, cod2, cod4
@@ -14,22 +16,20 @@ const GITHUB_API = new URL('/repos/cato-a/CODPM-MapImages/git/trees/main?recursi
 let updated = 0;
 
 const mapimages = {
-    'cod1+coduo': {'stock': [], 'custom': []},
-    'cod2': {'stock': [], 'custom': []},
-    'cod4': {'stock': [], 'custom': []}
+    'cod1+coduo': { 'stock': [], 'custom': [] },
+    'cod2': { 'stock': [], 'custom': [] },
+    'cod4': { 'stock': [], 'custom': [] }
 };
 
-function monotone(text)
-{
+function monotone(text) {
     return text.replace(/\^(?:\^\d)?\d/g, '');
 }
 
-function truncate(text, maxlength, postfix)
-{
-    if(postfix === undefined)
+function truncate(text, maxlength, postfix) {
+    if (postfix === undefined)
         postfix = '...';
 
-    if(text.length > maxlength)
+    if (text.length > maxlength)
         return text.substring(0, maxlength - postfix.length) + postfix;
 
     return text;
@@ -46,10 +46,10 @@ function ansicolorize(text, fg = 0, bg = 0, t1 = 0, t2) // syntax: <format>;<col
     Background:
     40: Firefly dark blue, 41: Orange, 42: Marble blue, 43: Greyish turquoise
     44: Gray, 45: Indigo, 46: Light gray, 47: White*/
-    if(text !== undefined && (typeof text === 'string' || text instanceof String)) {
+    if (text !== undefined && (typeof text === 'string' || text instanceof String)) {
         const esc = '\u001b';
         let color = `[${t1};${bg}`; // syntax: [<t1>;<bg>;<t2>;<fg>m
-        if(t2 !== undefined)
+        if (t2 !== undefined)
             color += `;${t2}`;
         color += `;${fg}m`;
         text = `${esc}${color}${text}${esc}[0m`;
@@ -58,21 +58,19 @@ function ansicolorize(text, fg = 0, bg = 0, t1 = 0, t2) // syntax: <format>;<col
     return text;
 }
 
-function pad(text, start, end, pad = ' ')
-{
-    if(start !== undefined && end !== undefined)
+function pad(text, start, end, pad = ' ') {
+    if (start !== undefined && end !== undefined)
         text = pad.repeat(start) + text + pad.repeat(end);
 
     return text;
 }
 
-function mapimage(mapimage)
-{
+function mapimage(mapimage) {
     const game = CODPM_GAME == 'cod' || CODPM_GAME == 'coduo' ? 'cod1+coduo' : CODPM_GAME;
-    if(mapimages[game].stock.indexOf(mapimage) !== -1)
+    if (mapimages[game].stock.indexOf(mapimage) !== -1)
         return CONFIG_MAPIMG_URL + game + '/stock/' + mapimage + '.png';
 
-    if(mapimages[game].custom.indexOf(mapimage) !== -1)
+    if (mapimages[game].custom.indexOf(mapimage) !== -1)
         return CONFIG_MAPIMG_URL + game + '/custom/' + mapimage + '.png';
 
     return CONFIG_MAPIMG_URL + 'unknown.png';
@@ -80,13 +78,13 @@ function mapimage(mapimage)
 
 (async () => {
     try {
-        const github = await fetch(GITHUB_API, {"headers": {"Accept": "application/vnd.github.raw+json"}});
-        if(github.ok && github.status == 200) {
+        const github = await fetch(GITHUB_API, { "headers": { "Accept": "application/vnd.github.raw+json" } });
+        if (github.ok && github.status == 200) {
             const rawj = await github.json();
 
             rawj.tree.forEach((element) => {
                 const mapdata = element.path.match(/^(cod1\+coduo|cod[24])\/(stock|custom)\/(.+)\.png$/);
-                if(mapdata === null)
+                if (mapdata === null)
                     return;
 
                 const game = mapdata[1];
@@ -97,20 +95,20 @@ function mapimage(mapimage)
         } else {
             console.error(`github.com server error: ${github.status} - ${github.statusText}`);
         }
-    } catch(err) {
+    } catch (err) {
         console.error(`Unable to fetch ${GITHUB_API}:`, err);
     }
 
     setInterval((async () => {
         try { //const ignoresrvs = ['176.31.252.60:*', '157.90.181.156:9120'];
-            const codpm = await fetch(CODPM_API, {"headers": {"Accept": "application/json"}});
-            if(!codpm.ok || codpm.status != 200) {
+            const codpm = await fetch(CODPM_API, { "headers": { "Accept": "application/json" } });
+            if (!codpm.ok || codpm.status != 200) {
                 console.error(`cod.pm server error: ${codpm.status} - ${codpm.statusText}`);
                 return;
             }
 
             const rawj = await codpm.json();
-            if(rawj.masterlist_updated == updated)
+            if (rawj.masterlist_updated == updated)
                 return;
 
             updated = rawj.masterlist_updated;
@@ -118,20 +116,20 @@ function mapimage(mapimage)
             const [hlength, plength, glength, mlength, clength] = [30, 5, 5, 16, 21];
             const parsedsrvs = new Array();
 
-            for(let i = 0; i < rawj.servers.length; i++) {
+            for (let i = 0; i < rawj.servers.length; i++) {
                 const server = rawj.servers[i];
 
-                if(server.hidden > 0 || server.sv_maxclients > 72) // ignoresrvs.some((element) => element.match(new RegExp(`^${server.ip}:(?:\\*|${server.port})$`)))
+                if (server.hidden > 0 || server.sv_maxclients > 72) // ignoresrvs.some((element) => element.match(new RegExp(`^${server.ip}:(?:\\*|${server.port})$`)))
                     continue;
 
                 totalservers++;
                 const players = server.playerinfo.length - server.bots;
 
-                if(players > 0) {
+                if (players > 0) {
                     globalplayers += players;
                     globalservers++;
 
-                    if(globalservers > 10)
+                    if (globalservers > 10)
                         continue;
 
                     let sv_hostname = server.sv_hostname.replace(/[^\x20-\x7E]+/g, '').replace(/^\s+|\s+$|(\s)\1+/g, (m, c) => c ? c : '');
@@ -152,25 +150,25 @@ function mapimage(mapimage)
                     };
 
                     parsedsrvs.push(current);
-                    if(current.sv_hostname.length > mhlength)
+                    if (current.sv_hostname.length > mhlength)
                         mhlength = current.sv_hostname.length;
-                    if(current.clients.length > mplength)
+                    if (current.clients.length > mplength)
                         mplength = current.clients.length;
-                    if(current.g_gametype.length > mglength)
+                    if (current.g_gametype.length > mglength)
                         mglength = current.g_gametype.length;
-                    if(current.mapname.length > mmlength)
+                    if (current.mapname.length > mmlength)
                         mmlength = current.mapname.length;
-                    if(current.connect.length > mclength)
+                    if (current.connect.length > mclength)
                         mclength = current.connect.length;
                 }
             }
 
-            const payload_json = {"embeds": new Array()};
+            const payload_json = { "embeds": new Array() };
             let message = '';
 
-            if(parsedsrvs.length > 0) {
+            if (parsedsrvs.length > 0) {
                 mhlength = Math.abs(hlength - mhlength);
-                mplength = Math.abs(plength - mplength); 
+                mplength = Math.abs(plength - mplength);
                 mglength = Math.abs(glength - mglength);
                 mmlength = Math.abs(mlength - mmlength);
                 mclength = Math.abs(clength - mclength);
@@ -181,7 +179,7 @@ function mapimage(mapimage)
                 const cwidth = 32 - offset - mclength;
 
                 message += '```ansi\n';
-                if(DISCORD_TABLELAYOUT == 'unicode') {
+                if (DISCORD_TABLELAYOUT == 'unicode') {
                     message += `╔${'═'.repeat(hwidth)}╦${'═'.repeat(dwidth)}╦${'═'.repeat(cwidth)}╗\n`;
                     message += `║${pad(ansicolorize('Server Name', 35), Math.floor((hwidth - 11) / 2), Math.ceil((hwidth - 11) / 2))}`
                         + `║${pad(ansicolorize('Details', 35), Math.floor((dwidth - 7) / 2), Math.ceil((dwidth - 7) / 2))}`
@@ -193,9 +191,9 @@ function mapimage(mapimage)
                         + ` ${ansicolorize(pad('Connect', Math.floor((cwidth - 7) / 2), Math.ceil((cwidth - 7) / 2)), 37, 42, 1)}\n`;
                 }
 
-                for(let i = 0; i < parsedsrvs.length; i++) {
+                for (let i = 0; i < parsedsrvs.length; i++) {
                     const server = parsedsrvs[i];
-                    if(DISCORD_TABLELAYOUT == 'unicode') {
+                    if (DISCORD_TABLELAYOUT == 'unicode') {
                         message += `║ [${ansicolorize(server.country_isocode, 32)}] ${server.sv_hostname.padEnd(hlength - mhlength)}`
                             + ` ║ ${server.clients.padStart(plength - mplength)} ${server.g_gametype.padEnd(glength - mglength)} ${server.mapname.padEnd(mlength - mmlength)}`
                             + ` ║ /connect ${server.connect.padEnd(clength - mclength)} ║\n`;
@@ -206,7 +204,7 @@ function mapimage(mapimage)
                     }
 
                     const servernum = i + 1;
-                    if(!DISCORD_MAXEMBEDS || servernum > DISCORD_MAXEMBEDS) continue; // only up to DISCORD_MAXEMBEDS
+                    if (!DISCORD_MAXEMBEDS || servernum > DISCORD_MAXEMBEDS) continue; // only up to DISCORD_MAXEMBEDS
                     payload_json.embeds.push({
                         "type": "rich",
                         "title": `\`#${servernum} ${truncate(server.sv_hostname, 22).padEnd(22)}\``,
@@ -230,7 +228,7 @@ function mapimage(mapimage)
                     });
                 }
 
-                if(DISCORD_TABLELAYOUT == 'unicode')
+                if (DISCORD_TABLELAYOUT == 'unicode')
                     message += `╚${'═'.repeat(hwidth)}╩${'═'.repeat(dwidth)}╩${'═'.repeat(cwidth)}╝\n`;
                 message += '```\n';
             }
@@ -241,29 +239,33 @@ function mapimage(mapimage)
             message += `> **There ${globalplayers != 1 ? 'are' : 'is'} currently ${globalplayers} player${globalplayers != 1 ? 's' : ''}`
                 + ` on ${globalservers} of ${totalservers} server${totalservers != 1 ? 's' : ''}`
                 + ` on [${CODPM_GAME == 'cod' ? 'COD1' : CODPM_GAME.toUpperCase()} v${CODPM_VERSION}](https://cod.pm?game=${CODPM_GAME}&version=${CODPM_VERSION})`;
-            if(DISCORD_MAXEMBEDS && parsedsrvs.length > 0)
+            if (DISCORD_MAXEMBEDS && parsedsrvs.length > 0)
                 message += `\n> Below ${globalservers == 1 ? 'is the' : 'are the top'} ${globalservers < DISCORD_MAXEMBEDS ? (globalservers == 1 ? 'only' : globalservers) : DISCORD_MAXEMBEDS} currently active server${totalservers != 1 ? 's' : ''}.`;
             message += `\n> Updated: <t:${timestamp}:R>**`;
             payload_json.content = message;
 
             try {
-                const form = new FormData();
-                form.append('payload_json', JSON.stringify(payload_json));
 
-                const discord = await fetch(DISCORD_WEBHOOK, {
-                    "method": /\/messages\/\d+$/.test(DISCORD_WEBHOOK) ? "PATCH" : "POST",
-                    "body": form
-                });
+                if (!LAST_MESSAGE_ID) {
+                    const message = await DISCORD_WEBHOOK.send({ content: payload_json.content, embeds: payload_json.embeds });
 
-                if(!discord.ok) {
-                    console.error(`Server error: ${discord.status} - ${discord.statusText}`);
-                    console.error(await discord.json());
-                    updated = 0;
+                    if (!message) {
+                        updated = 0;
+                    }
+
+                    LAST_MESSAGE_ID = message.id;
+                } else {
+                    const updatedMessage = await DISCORD_WEBHOOK.editMessage(LAST_MESSAGE_ID, { content: payload_json.content, embeds: payload_json.embeds });
+
+                    if (!updatedMessage) {
+                        updated = 0;
+                    }
                 }
-            } catch(err) {
-                console.error(`Unable to fetch ${DISCORD_WEBHOOK}:`, err);
+
+            } catch (err) {
+                console.error(`Unable to send/updated the message:`, err);
             }
-        } catch(err) {
+        } catch (err) {
             console.error(`Unable to fetch ${CODPM_API}:`, err);
         }
     }), 10 * 1000);
